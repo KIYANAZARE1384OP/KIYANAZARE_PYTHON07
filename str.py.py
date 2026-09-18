a82ace2d972f1073f1c355d6661ffa00a589eb4b
KIYANAZARE1384OP <KIYANA.ZARE.13@GMAIL.COM>
2026-09-18 13:51:50 +0330

    Add files via upload

---
+++ str.py.py
@@ -0,0 +1,19 @@
+jomle1="hello world"
+jomle2="in the name of god"
+jomle3="i am kiyana zare"
+
+
+print(jomle1)
+print(jomle2)
+print(jomle3)
+
+
+print(type(jomle1))
+print(type(jomle2))
+print(type(jomle3))
+
+
+print(len(jomle1))
+print(len(jomle2))
+print(len(jomle3))
+ 
