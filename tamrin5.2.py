cbd9e85df98ae92c62f7fe92efddc093eaa4db19
KIYANAZARE1384OP <KIYANA.ZARE.13@GMAIL.COM>
2026-09-18 13:58:01 +0330

    Add files via upload

---
+++ tamrin5.2.py
@@ -0,0 +1,37 @@
+username="admin"
+password=1234
+count=0
+while count<3:
+    user_name=input("esmet ra vared kon")
+    user_password=input("passwordet ra vared kon")
+    if username!=user_name or password!=user_password:
+        print("username ya password eshtebahe")
+        count+=1
+        continue
+    print("ba movafaghiyat vared shhodid")
+    break
+else:
+    print("accounte shoma ghofl shod")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+foods=["pizza","burger","pasta","salad"]
+print(foods)
+orders=[]
+while True :
+    user=input("esme ghazaye mored nazar ra vared konid")
+    if user=="order":
+        break
+    if user in foods:
+        order.append(user)
+        print ("factor")
+        for i in orders:
+            print(i)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print("salam")
+while True:
+    user=input("ye chizi benevisid")
+    if user=="bye"
+    print("good bye")
+    break
+print("javabe chat bot") 
+    
+        
