465e4130f055d00683917696dd3dfd827441e0fc
KIYANAZARE1384OP <KIYANA.ZARE.13@GMAIL.COM>
2026-09-18 13:56:50 +0330

    Add files via upload

---
+++ tamrin4.2.py
@@ -0,0 +1,49 @@
+#q4.7
+numbers=[1,7,15,23,31,44]
+square_of_numbers=[]
+
+for num in numbers:
+    square_of_numbers.append(num**2)
+    print(square_of_numbers)
+#q4.8
+users=["amir,ali,vahid,hamidreza,mohamadreza"]
+count=0
+for name in users :
+    if len (name)<5:
+        count=count+1
+print(count)
+#q4.9
+buy_price=[100,200,150,400]
+sell_price=[130,250,190,500]
+
+profits=[]
+
+for i in range (len(by_pricees)):
+    profit=sell_prices[i]_buy_prices[i]
+    profit.append(profit)
+    print(profits)
+
+#4..10
+celsius_temps=[]
+
+for temp in celsius_temps:
+    farenheit_temps.append(temp*1.8+32)
+print(farenheit_temps)
+
+#q4.12
+shopping_basket+[]
+
+for i in range (5):
+    product=input("name mahsool ra vared konid")
+    if len (product)<6:
+        shopping_basket.append(product)
+print(shopping_basket)
+        
+        
+
+
+
+
+
+
+
